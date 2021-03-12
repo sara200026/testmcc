afa6b192ecbd28faf2ca4567ba473e05175eae88
sara200026 <ayashnj@gmail.com>
2021-03-12 10:13:41 +0200

    add sara

--- app/src/main/java/com/example/mcc/MainActivity.java
+++ app/src/main/java/com/example/mcc/MainActivity.java
@@ -12,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("TAG","onCreate: wellcome");
+        Log.d("TAG","onCreate: wellcome sara ");
+
     }
 }
